bfc1b01827bb2a66ed41faf8e7bc7eead027dc03
Catastrophe0123 <vnavaneeth15@gmail.com>
2020-01-25 22:08:18 +0530

    added reading and posting new Screams

--- backend/routes/api/screams.js
+++ backend/routes/api/screams.js
@@ -4,18 +4,35 @@ const Screams = require('../../models/Screams');
 // routes for www.site.com/api/screams
 
 // get all screams
-router.get('/', async function(req, res) {
+router.get('/', function(req, res) {
     // to get all the screams
     //check for authorisation later
     //find all the screams from the db and return the values
     //assume the db to be called Screams
 
-    Screams.find({}, function(err, res) {
+    Screams.find({}, function(err, allScreams) {
         //found all the screams
-        res.status(200).json(res);
+        return res.status(200).json(allScreams);
     });
+});
 
-    res.send('hello world from /api/screams');
+router.post('/', async (req, res) => {
+    //post a new scream to the db
+    const newScream = {
+        userHandle: req.body.userHandle,
+        body: req.body.body
+    };
+    try {
+        const Scream = await Screams.create(newScream);
+        return res
+            .status(201)
+            .json(`message: created new Scream with id = ${Scream._id}`);
+    } catch (err) {
+        console.log('error : ', err);
+        return res
+            .status(500)
+            .json('message: server error. Could not create Scream');
+    }
 });
 
 module.exports = router;
