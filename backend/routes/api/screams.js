3d314c331865f84f476ffb12421d80596503ae24
Catastrophe0123 <vnavaneeth15@gmail.com>
2020-03-28 12:50:07 +0530

    refactored like routes

--- backend/routes/api/screams.js
+++ backend/routes/api/screams.js
@@ -121,7 +121,7 @@ router.post('/:screamId/comment', isAuth, async (req, res) => {
 	}
 });
 
-router.put('/:screamId/like', isAuth, async (req, res) => {
+router.post('/:screamId/like', isAuth, async (req, res) => {
 	// we have the user
 	// we have the scream
 
@@ -130,44 +130,67 @@ router.put('/:screamId/like', isAuth, async (req, res) => {
 		if (!scream) {
 			return res.status(404).json({ message: 'scream not found' });
 		}
+
 		// we got the scream
 		console.log(scream.likes);
-		let likedUser = scream.likes.filter(user => user == req.user.id);
-		console.log('liked user :', likedUser);
-		console.log('liked user :', likedUser.length);
-		if (likedUser.length === 0) {
-			scream.likes.push(req.user);
-			await scream.save();
-			return res.status(200).json({ message: 'liked successfully' });
+
+		let foundUserLike = scream.likes.some(function(like) {
+			return like.equals(req.user.id);
+		});
+
+		let message = '';
+
+		if (foundUserLike) {
+			// user already liked, removing like
+			scream.likes.pull(req.user.id);
+			message = 'unliked successfully';
 		} else {
-			return res.status(400).json({ message: 'cannot like twice' });
+			// adding the new user like
+			scream.likes.push(req.user.id);
+			message = 'liked successfully';
 		}
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: 'server error', err });
-	}
-});
 
-router.put('/:screamId/unlike', isAuth, async (req, res) => {
-	try {
-		//we have the scream id
-		// we have the user
-		const scream = await Scream.findById(req.params.screamId);
-		if (!scream) {
-			return res.status(404).json({ message: 'scream not found' });
-		}
-		console.log('before splice: ', scream.likes);
-		console.log(req.user.id);
-		scream.likes.splice(scream.likes.indexOf(req.user.id), 1); // removed from scream
 		await scream.save();
-		console.log('after splice : ', scream.likes);
+		return res.status(200).json({ message, scream });
 
-		return res.status(200).json({ message: 'unliked successfully' });
+		// OLD CODE
+		// let likedUser = scream.likes.filter(user => user == req.user.id);
+		// console.log('liked user :', likedUser);
+		// console.log('liked user :', likedUser.length);
+		// if (likedUser.length === 0) {
+		// 	scream.likes.push(req.user);
+		// 	await scream.save();
+		// 	return res.status(200).json({ message: 'liked successfully' });
+		// } else {
+		// 	return res.status(400).json({ message: 'cannot like twice' });
+		// }
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ message: 'server error', err });
 	}
 });
+
+// OLD CODE
+// router.put('/:screamId/unlike', isAuth, async (req, res) => {
+// 	try {
+// 		//we have the scream id
+// 		// we have the user
+// 		const scream = await Scream.findById(req.params.screamId);
+// 		if (!scream) {
+// 			return res.status(404).json({ message: 'scream not found' });
+// 		}
+// 		console.log('before splice: ', scream.likes);
+// 		console.log(req.user.id);
+// 		scream.likes.splice(scream.likes.indexOf(req.user.id), 1); // removed from scream
+// 		await scream.save();
+// 		console.log('after splice : ', scream.likes);
+
+// 		return res.status(200).json({ message: 'unliked successfully' });
+// 	} catch (err) {
+// 		console.error(err);
+// 		res.status(500).json({ message: 'server error', err });
+// 	}
+// });
 
 router.delete('/:screamId/', isAuth, async (req, res) => {
 	// delete the scream based on the scream id
@@ -195,42 +218,68 @@ router.delete('/:screamId/', isAuth, async (req, res) => {
 	}
 });
 
-router.put('/:screamId/:commentId/like', isAuth, async (req, res) => {
+router.post('/:screamId/:commentId/like', isAuth, async (req, res) => {
 	//like a comment
 
 	try {
 		const comment = await Comment.findById(req.params.commentId);
-		const likedUser = comment.likes.filter(user => user === req.user.id);
-		if (likedUser.length === 0) {
-			comment.likes.push(req.user);
-			await comment.save();
-			return res.status(200).json({ message: 'liked successfully' });
-		} else {
-			return res.status(400).json({ message: 'cannot like twice' });
+
+		if (!comment) {
+			return res.status(404).json({ message: 'scream not found' });
 		}
+
+		let foundUserLike = comment.likes.some(function(like) {
+			return like.equals(req.user.id);
+		});
+
+		let message = '';
+
+		if (foundUserLike) {
+			// user already liked, removing like
+			comment.likes.pull(req.user.id);
+			message = 'unliked successfully';
+		} else {
+			// adding the new user like
+			comment.likes.push(req.user.id);
+			message = 'liked successfully';
+		}
+
+		await comment.save();
+		return res.status(200).json({ message, comment });
+
+		// OLD CODE
+		// const likedUser = comment.likes.filter(user => user === req.user.id);
+		// if (likedUser.length === 0) {
+		// 	comment.likes.push(req.user);
+		// 	await comment.save();
+		// 	return res.status(200).json({ message: 'liked successfully' });
+		// } else {
+		// 	return res.status(400).json({ message: 'cannot like twice' });
+		// }
 	} catch (err) {
 		console.error(err);
 		return res.status(500).json({ message: 'server error', err });
 	}
 });
 
-router.put('/:screamId/:commentId/unlike', isAuth, async (req, res) => {
-	try {
-		//we have the scream id
-		// we have the user
-		const comment = await Comment.findById(req.params.commentId);
-		if (!comment) {
-			return res.status(404).json({ message: 'comment not found' });
-		}
-		comment.likes.splice(comment.likes.indexOf(req.user.id), 1); // removed from scream
-		await comment.save();
+// OLD CODE
+// router.put('/:screamId/:commentId/unlike', isAuth, async (req, res) => {
+// 	try {
+// 		//we have the scream id
+// 		// we have the user
+// 		const comment = await Comment.findById(req.params.commentId);
+// 		if (!comment) {
+// 			return res.status(404).json({ message: 'comment not found' });
+// 		}
+// 		comment.likes.splice(comment.likes.indexOf(req.user.id), 1); // removed from scream
+// 		await comment.save();
 
-		return res.status(200).json({ message: 'unliked successfully' });
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: 'server error', err });
-	}
-});
+// 		return res.status(200).json({ message: 'unliked successfully' });
+// 	} catch (err) {
+// 		console.error(err);
+// 		res.status(500).json({ message: 'server error', err });
+// 	}
+// });
 
 router.delete('/:screamId/:commentId/', isAuth, async (req, res) => {
 	//delete the comment
